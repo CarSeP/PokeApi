6531830c524c3186d249e83759499379589b61b9
CarSeP <vjpd64@gmail.com>
2025-01-29 21:44:41 -0400

    Add card design and a data fetch when entering the site

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
+import { useEffect, useState } from "react";
+import Card from "./components/card";
+import axios from "axios";
+
 function App() {
+  const [pokemons, setPokemon] = useState<any[]>([]);
+
+  const getPokemons = async () => {
+    const pokemonList = []
+    for (let i = 1; i < 152; i++) {
+      const url = "https://pokeapi.co/api/v2/pokemon/" + i;
+      const { data } = await axios.get(url);
+      pokemonList.push(data)
+    }
+    setPokemon(pokemonList)
+  };
+
+  useEffect(() => {
+    getPokemons();
+  }, []);
+
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold text-center text-blue-600 mb-8">
+        Pokemon API
+      </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {
+          pokemons.map((el) => <Card pokemon={el} key={el.id }/>)
+        }
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/Card.tsx
@@ -0,0 +1,28 @@
+function Card({ pokemon }: { pokemon: any }) {
+  return (
+    <div className="bg-white rounded-lg shadow-md p-6 cursor-pointer transition-transform duration-300 hover:translate-y-[-1rem]">
+        <h3 className="text-xl text-gray-500 italic">#{pokemon.id}</h3>
+      <img
+        src={pokemon.sprites.front_default}
+        className="w-32 h-32 mx-auto mb-4"
+      />
+      <h2 className="text-xl font-semibold text-center mb-2">{pokemon.name}</h2>
+      <div className="flex justify-center space-x-2">
+        {pokemon.types.map((el: any) => (
+          <span
+            key={el.slot}
+            className={`${el.type.name} text-xs font-semibold px-2 py-1 rounded`}
+          >
+            {el.type.name}
+          </span>
+        ))}
+      </div>
+      <div className="text-sm text-gray-600 text-center mt-4">
+        <p>Weight: {pokemon.weight} kg</p>
+        <p>Height: {pokemon.height}5 m</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
