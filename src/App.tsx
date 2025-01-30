8b1e1daa3c66286556b8b7cc4c7a6963c876685b
CarSeP <vjpd64@gmail.com>
2025-01-29 21:53:02 -0400

    Add loader

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import axios from "axios";
 
 function App() {
   const [pokemons, setPokemon] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const getPokemons = async () => {
-    const pokemonList = []
+    const pokemonList = [];
     for (let i = 1; i < 152; i++) {
       const url = "https://pokeapi.co/api/v2/pokemon/" + i;
       const { data } = await axios.get(url);
-      pokemonList.push(data)
+      pokemonList.push(data);
     }
-    setPokemon(pokemonList)
+    setPokemon(pokemonList);
+    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -24,11 +26,22 @@ function App() {
       <h1 className="text-4xl font-bold text-center text-blue-600 mb-8">
         Pokemon API
       </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {
-          pokemons.map((el) => <Card pokemon={el} key={el.id }/>)
-        }
-      </div>
+      {!isLoading && (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {pokemons.map((el) => (
+            <Card pokemon={el} key={el.id} />
+          ))}
+        </div>
+      )}
+      {isLoading && (
+        <div className="fixed top-0 left-0 w-full h-screen flex">
+          <div className="flex-col gap-4 w-full flex items-center justify-center">
+            <div className="w-20 h-20 border-4 border-transparent text-blue-400 text-4xl animate-spin flex items-center justify-center border-t-blue-400 rounded-full">
+              <div className="w-16 h-16 border-4 border-transparent text-red-400 text-2xl animate-spin flex items-center justify-center border-t-red-400 rounded-full"></div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
