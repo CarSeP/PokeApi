a1c9b77ff81298e57785aa38511ee5dca8917221
CarSeP <vjpd64@gmail.com>
2025-01-31 19:50:05 -0400

    Refactoring of stats color change function

--- src/components/PokemonStat.tsx
+++ src/components/PokemonStat.tsx
@@ -1,26 +1,15 @@
 import { capitalizeFirstLetter } from "../utils/capitalizeWords";
 
-type StatType = "hp" | "attack" | "defense" | "speed" | "special-attack" | "special-defense";
-
 function PokemonStat({ stat }: { stat: any }) {
   const width = {
     width: `${(stat.base_stat / 255) * 100}%`,
-  };
-
-  const styles = {
-    hp: "bg-green-500",
-    attack: "bg-red-500",
-    defense: "bg-blue-500",
-    speed: "bg-yellow-500",
-    "special-attack": "bg-purple-500",
-    "special-defense": "bg-indigo-500"
   };
 
   return (
     <div>
       <p className="font-semibold">{capitalizeFirstLetter(stat.stat.name)}</p>
       <div className="bg-gray-200 rounded-full h-4 mt-1">
-      <div className={`rounded-full h-4 ${styles[stat.stat.name as StatType]}`} style={width}></div>
+      <div className={`rounded-full h-4 stat-${stat.stat.name}`} style={width}></div>
       </div>
       <p className="text-sm text-right">{stat.base_stat}</p>
     </div>
